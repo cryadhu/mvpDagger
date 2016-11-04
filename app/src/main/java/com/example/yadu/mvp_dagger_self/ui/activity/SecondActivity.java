77e306797f52dee6d25ab2048d56ba4e36b6ecd3
Amal <amal@ot.com>
2016-11-04 17:50:51 +0530

    hey

---
+++ app/src/main/java/com/example/yadu/mvp_dagger_self/ui/activity/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.yadu.mvp_dagger_self.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yadu.mvp_dagger_self.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
