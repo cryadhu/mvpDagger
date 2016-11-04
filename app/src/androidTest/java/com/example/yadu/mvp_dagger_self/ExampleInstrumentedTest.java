06afcf6c1d14cc5acee1f9f1bd690f0ea17b35c8
Amal <amal@ot.com>
2016-11-04 17:50:04 +0530

    hey

--- app/src/androidTest/java/com/example/yadu/mvp_dagger_self/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/yadu/mvp_dagger_self/ExampleInstrumentedTest.java
@@ -2,11 +2,20 @@ package com.example.yadu.mvp_dagger_self;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.yadu.mvp_dagger_self.ui.activity.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +25,18 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        onView(withId(R.id.button)).perform(click());
 
-        assertEquals("com.example.yadu.mvp_dagger_self", appContext.getPackageName());
+        // Check if the add note screen is displayed
+        onView(withId(R.id.textView2)).check(matches(isDisplayed()));
     }
 }
--- app/src/main/java/com/example/yadu/mvp_dagger_self/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/yadu/mvp_dagger_self/ui/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.yadu.mvp_dagger_self.ui.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.yadu.mvp_dagger_self.MyApplication;
 import com.example.yadu.mvp_dagger_self.R;
@@ -21,6 +24,9 @@ public class MainActivity extends AppCompatActivity implements MainScreenContrac
 
     @Inject
     MainScreenPresenter mainScreenPresenter;
+
+
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +37,20 @@ public class MainActivity extends AppCompatActivity implements MainScreenContrac
                 .build().inject(this);
 
         mainScreenPresenter.loadData();
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(),SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     public void showList(List<Post> posts) {
         Log.e("datafff",posts.get(0).getBody());
+
     }
 
     @Override
